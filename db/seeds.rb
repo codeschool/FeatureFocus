ef1981a621c262b0a21d8c8c6f29a82e242df63f
Carlos Souza <carloshrsouza@gmail.com>
2014-06-02 15:48:05 -0400

    Base style and 2nd project.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,8 @@ message = project.messages.create!(title: 'Programming Languages')
 message.comments << Comment.create!(body: 'Ruby should be in there')
 message.comments << Comment.create!(body: 'JavaScript should be there too!')
 
+project = owner.owned_projects.create!(title: 'How to Cook')
+message = project.messages.create!(title: 'Operating a microwave')
+message.comments << Comment.create!(body: 'Mastering the numbers is the first step.')
+message.comments << Comment.create!(body: 'How long does popcorn take ?')
+
