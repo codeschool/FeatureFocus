26479f5b3b0c05272fd73eb7ed8b3ca92f6853a2
Carlos Souza <carloshrsouza@gmail.com>
2014-06-04 16:02:15 -0400

    Better routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-
-  resources :projects, only: [:index, :show]
-  resources :messages
-  resources :comments
+  resources :projects, only: [:index, :show], shallow: true do
+    resources :messages
+    resources :comments
+  end
 
   get 'progress' => 'progress#show'
 
