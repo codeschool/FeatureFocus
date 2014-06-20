cd56a5dc49c98adb7bdd07aad24457c968eb381a
Carlos Souza <carloshrsouza@gmail.com>
2014-06-20 09:15:06 -0400

    Update port and fix formating.

--- threadless.js
+++ threadless.js
@@ -7,22 +7,22 @@ var Threadless = {
 
     var template = Handlebars.compile($('#notification-entries').html());
     function loadNotifications(data) {
-      console.log('Notifications: ' + data);
-      $('<ul/>', {'class': 'notifications', html: template(data)}).appendTo('.notifications-list');
+      $('<ul/>', {'class': 'notifications', html: template(data)}).
+        appendTo('.notifications-list');
     }
 
-    $.get('http://localhost:3000/notifications', loadNotifications);
+    $.get('http://localhost:3001/notifications', loadNotifications);
 
-    // TODO add cookies ?
     $('.points').on('click', 'li', function(e){
       var target = $(e.target);
       var designId = target.closest('ul').data('design-id');
       var score = target.closest('li').find('a').text();
 
-      $.post('http://localhost:3000/designs/' + designId + '/votes', { score: score }, voteCasted);
+      $.post('http://localhost:3001/designs/' + designId + '/votes', { score: score },
+             voteCast);
     });
 
-    function voteCasted() {
+    function voteCast() {
       console.log('done!')
     }
   }
